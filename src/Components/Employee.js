55a8a7cfdae08bde0b9ab72a480c6c309edca7f8
Larry Arrington <larryarrington@pursuit.org>
2023-05-10 18:26:29 -0400

    lifecyclesandnetworking

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,13 +1,56 @@
+import { useState, useEffect } from "react";
 import PetList from "./PetList";
 import "./Employee.css";
 
-export const Employee = () => {
+export const Employee = ({ employee }) => {
+  const [showPets, setShowPets] = useState([]);
+  const [toggle, setToggle] = useState(false);
+  const { id, firstName, lastName, prefix, postfix, title } = employee;
+  const fullName = `${prefix ? prefix + " " : ""}${firstName} ${lastName}${
+    postfix ? ", " + postfix : ""
+  }`;
+
+  async function fetchAPI() {
+    try {
+      const response = await fetch(
+        "https://backend-vet-api.onrender.com/api/pets"
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return console.log("Error fetching API: ", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchAPI()
+      .then((response) => {
+        setShowPets(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  function handleToggle() {
+    setToggle(!toggle);
+  }
+
+  function filterPets() {
+    const pets = showPets.filter((eachPet) => {
+      return eachPet.employeeId === id;
+    });
+    return <PetList key={pets.id} pets={pets} />;
+  }
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+      <h3>{fullName}</h3>
+      <h4>{title}</h4>
+      <button onClick={() => handleToggle()}>
+        {toggle ? "Hide Pets" : "Show Pets"}
+      </button>
+      {toggle ? filterPets() : null}
     </article>
   );
 };
