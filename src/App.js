1376b831f8bad0558db67ebcf58ba0be2664e87c
Hillary Brito <hillarybrito@hillarys-air.myfiosgateway.com>
2023-05-11 04:18:09 -0400

    Cypress test failed despite info printing into card onClick

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
 
+
 function App () {
   return (
     <>
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,7 +1,33 @@
-export const PetList = () => {
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const PetList = ({ employeeId }) => {
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    fetchPets();
+  }, []);
+
+  const fetchPets = async () => {
+    try {
+      const response = await axios.get(
+        `https://backend-vet-api.onrender.com/api/employees/${employeeId}/pets`
+      );
+      setPets(response.data);
+    } catch (error) {
+      console.error("Error fetching pets:", error);
+    }
+  };
+
+  const petNames = pets.map((pet) => pet.name).join(", ");
+
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {pets.length > 0 ? (
+        <p>{petNames}</p>
+      ) : (
+        <p>No pets listed for this employee.</p>
+      )}
     </aside>
   );
 };
