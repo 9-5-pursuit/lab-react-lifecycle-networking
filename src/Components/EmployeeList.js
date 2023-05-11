f81f9ef2baa6754ebd7611bc99d7b5aefe6b0094
Amina-Moufakkir <aminamouf22@gmail.com>
2023-05-11 08:22:06 -0400

    useEffect lab

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { useFetch } from "./useFetch";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
 export const EmployeeList = () => {
+  const {
+    data: employees,
+    isLoading,
+    error,
+  } = useFetch(`https://vet-api-1.onrender.com/api/employees`);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error fetching employees: {error.message}</div>;
+  }
+
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {employees.map((employee) => (
+          <Employee key={employee.id} {...employee} />
+        ))}
       </section>
     </main>
   );
