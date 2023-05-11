acfc8918f827e4babba2053225e9ea93db3ec37e
Angel <angel@Angels-MacBook-Air.local>
2023-05-11 09:53:44 -0400

    react life-cycle networking lab

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
 export const EmployeeList = () => {
+  const [employee, setEmployee] = useState([]);
+
+  useEffect(() => {
+    fetch("https://backend-vet-api.onrender.com/api/employees")
+      .then((response) => response.json())
+      .then((data) => setEmployee(data));
+  }, []);
+
+
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        <Employee employee={employee} />
       </section>
     </main>
   );
