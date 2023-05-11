be7cbce038184ed26d5b2620429f643073838f35
Nicole Rodriguez <nicolerodriguez@nicoles-air.mynetworksettings.com>
2023-05-11 05:28:54 -0400

    liifecycle-lab rendered

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,7 +1,38 @@
-export const PetList = () => {
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export const PetList = ({ employee }) => {
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    fetchPets();
+  }, []);
+
+  async function fetchPets() {
+    try {
+      const response = await axios.get(
+        "https://backend-vet-api.onrender.com/api/pets"
+      );
+      setPets(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Find pets associated with the employee
+  const employeePets = pets.filter((pet) => pet.employeeId === employee.id);
+
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {employeePets.length === 0 ? (
+        <p>No pets listed for this employee.</p>
+      ) : (
+        <ul>
+          {employeePets.map((pet) => (
+            <li key={pet.id}>{pet.name}</li>
+          ))}
+        </ul>
+      )}
     </aside>
   );
 };
