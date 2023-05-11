82ab64ce14d9a81cbefc34b4f102c808f4416993
davep80 <davidpaq1@gmail.com>
2023-05-11 11:40:10 -0400

    refactor useEffect

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,24 +1,24 @@
 import React from "react";
 import Employee from "./Employee";
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import "./EmployeeList.css";
 export const EmployeeList = () => {
 
-  const [data, setData] = useState(null)
+  const [data, setData] = useState([])
 
   useEffect(() => {
     getData()
   }, [])
 
   async function getData() {
-    fetch('https://backend-vet-api.onrender.com/api/employees').then(data => data.json() ).then(data => setData(data))
+    fetch('https://vet-api-1.onrender.com/api/employees').then(data => data.json()).then(data => setData(data))
   }
-  
+
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-       {data && <Employee data={data}/>}
+        <Employee data={data} />
       </section>
     </main>
   );
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,23 +1,21 @@
 import { useEffect, useState } from "react";
 export const PetList = ({ data }) => {
 
-  const [pets, setPets] = useState(null)
+  const [pets, setPets] = useState([])
   useEffect(() => {
     getPets(data)
-  })
+  }, [])
   async function getPets(args) {
 
-    fetch('https://backend-vet-api.onrender.com/api/pets').then(item => item.json())
-    .then(item => setPets(item.filter(item => item.employeeId === args.id).map(item => {
-      return item.name
-    }).join(',') || 'No pets listed'))
+    fetch('https://vet-api-1.onrender.com/api/pets').then(item => item.json())
+      .then(item => setPets(item.filter(item => item.employeeId === args.id).map(item => {
+        return item.name
+      }).join(',') || 'No pets listed'))
 
   }
   return (
     <aside className="pets-list">
-      {pets &&
-        <p>{pets}</p>
-      }
+      <p>{pets}</p>
     </aside>
   );
 };
