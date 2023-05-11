52f5ac6d9bbc2ef55eb97f3e418cada203891fd7
Fabio Tavarez <fabiotavarez@pursuit.org>
2023-05-11 02:53:00 -0400

    Employees info pops up dynamic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
+//import axios from "axios";
 
 function App () {
+  // const api = "https://backend-vet-api.onrender.com";
+  // const employees = "/api/employees";
+  // const pets = "/api/pets"
+
+  // useEffect(() => {
+  //   fetchAPIEmployee();
+  // }, [])
+  
+
+  // async function fetchAPIEmployee(){
+  //   try {
+  //     const result = await axios.get(
+  //       `https://backend-vet-api.onrender.com${employees}`
+  //     );
+  //     console.log(result.data)
+      
+  //   } catch (e) {
+  //     console.log(e)
+  //   }
+  // }
+
   return (
     <>
       <NavBar />
--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,11 +1,26 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export const Employee = () => {
+export const Employee = ({ item }) => {
+  const [showData, setShowData] = useState(false);
+  const pets = "/api/pets";
+  console.log(item);
+  let first = item.firstName;
+  let last = item.lastName;
+  if (item.prefix) {
+    first = `${item.prefix} ${item.firstName}`;
+  }
+
+  if (item.postfix) {
+    last = `${item.lastName}, ${item.postfix}`;
+  }
+
   return (
-    <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
+    <article className="employee" key={item.id}>
+      <h3>{`${first} ${last}`}</h3>
+      <h4>{item.title}</h4>
       <button>Show Pets</button>
       <PetList />
     </article>
@@ -13,3 +28,15 @@ export const Employee = () => {
 };
 
 export default Employee;
+
+// let employeeList = employee.map((item)=>{
+//   let formattedName = `${item.prefix}${item.firstName}${item.lastName}${item.postfix}`;
+//   <>
+//     <article className="employee" key={item}>
+//       <h3>{formattedName}</h3>
+//       <h4>{item.title}</h4>
+//       <button>Show Pets</button>
+//       <PetList />
+//     </article>
+//   </>;
+// })
