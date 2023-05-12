6bc44e3ea3eeaccdece4b5a76c22561ddd7fad30
thandi bristol <thandibristol@thandis-air.mynetworksettings.com>
2023-05-12 00:11:14 -0400

    done

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
 export const EmployeeList = () => {
+  
+  const [employees, setemployees] = useState([])
+  
+  
+    async function getData(){
+      try{
+        const result = await axios.get(
+          `https://vet-api-1.onrender.com/api/employees`
+        );
+        setemployees(result.data);
+      }catch(e){} 
+    }
+  
+
+  useEffect(()=>{
+    getData();
+  },[]);
+  
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {employees.map((item)=> {
+          return  <Employee key={item.id} {...item}/>
+        })}
+       
       </section>
     </main>
   );
-};
+}
 
 export default EmployeeList;
