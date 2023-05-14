994e27c89ab286861e76b35a3a0f11f2b87df9af
Mussie <mussierussom@pursuit.org>
2023-05-14 18:53:07 -0400

    React lifecycle-networking lab is finish.

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
 export const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+
+  async function getData() {
+    try {
+      const result = await axios.get(
+        `https://vet-api-1.onrender.com/api/employees`
+      );
+
+      // console.log(result.data);
+      setEmployees(result.data);
+    } catch (e) {}
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {employees.map((item) => {
+          return <Employee key={item.id} {...item} />;
+          // item={item} or {...item}
+        })}
       </section>
     </main>
   );
