ddc91d07530af50f1022e1679023f322af5dae4a
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-05-14 16:38:12 -0400

    Completed

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
 export const EmployeeList = () => {
+  // State
+  const [employees, setEmployees] = useState([]);
+
+  // Functions
+  async function getEmployeeeData(){
+    try{
+      const result = await axios.get('https://vet-api-1.onrender.com/api/employees');
+
+      setEmployees(result.data);
+    }
+    catch(e){
+      console.log(e)
+    }
+  }
+
+  // Calling useEffect
+  useEffect(() => {
+    getEmployeeeData();
+  }, [])
+
+
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {
+          employees.map((employee) => {
+            return(
+              <Employee key={employee.id} employee={employee}/>
+            )
+          })
+        }
       </section>
     </main>
   );
