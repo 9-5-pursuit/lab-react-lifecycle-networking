0e13f695a90de76590f79982e8aa6daf93487d41
Michelle Harley <chell.hx@gmail.com>
2023-05-15 00:16:28 -0400

    React - Lifecycle Networking Lab Done

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import PetList from "./PetList";
+
 import "./Employee.css";
 
-export const Employee = () => {
+export const Employee = ({
+  id,
+  firstName,
+  lastName,
+  prefix,
+  postfix,
+  title,
+}) => {
+  // state for show pets
+  const [click, setClick] = useState(false);
+
+  async function handleClick() {
+    setClick(!click);
+  }
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+      <h3>
+        {prefix ? prefix : ""} {firstName} {lastName}
+        {postfix ? `, ${postfix}` : null}
+      </h3>
+      <h4>{title}</h4>
+      <button
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        {!click ? "Show Pets" : "Hide Pets"}
+      </button>
+      {click && <PetList id={id} />}
     </article>
   );
 };
--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,27 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { handleAPI } from "./data/APIfunction";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
 export const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]); // API gives array data
+
+  useEffect(() => {
+    handleAPI("employees", setEmployees);
+  }, []);
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {employees.map((employee) => {
+          return (
+            <Employee
+              key={employee.id}
+              // item={item} - spread op to simplify below
+              {...employee}
+            />
+          );
+        })}
       </section>
     </main>
   );
