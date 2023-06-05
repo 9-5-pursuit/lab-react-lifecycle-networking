362ed5e9d34117e9ad7e162e741bf82382dd5505
Esmeralda Gonzalez Rojo <esmeralda@Esmeraldas-Air.lan>
2023-06-04 23:29:36 -0400

    done

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,7 +1,71 @@
-export const PetList = () => {
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+
+// const PetList = ({ id }) => {
+//   const [pet, setPet] = useState([]);
+
+//   console.log(id);
+
+//   useEffect(() => {
+//     fetchPets();
+//   }, []);
+
+//   const fetchPets = async () => {
+//     try {
+//       const response = await axios.get(
+//         `https://backend-vet-api.onrender.com/api/employees/${id}/pets`
+//       );
+//       setPet(response.data);
+//     } catch (e) {
+//       console.error("Error fetching pets:", e);
+//     }
+//   };
+
+//   const petNames = pet.map((pet) => pet.name).join(", ");
+
+//   return (
+//     <aside className="pets-list">
+//       {pet.length > 0 ? (
+//         <p>{petNames}</p>
+//       ) : (
+//         <p>No pets listed for this employee.</p>
+//       )}
+//     </aside>
+//   );
+// };
+
+// export default PetList;
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const PetList = ({ id }) => {
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    fetchPets();
+    // eslint-disable-next-line
+  }, [id]);
+
+  const fetchPets = async () => {
+    try {
+      const response = await axios.get(
+        `https://backend-vet-api.onrender.com/api/employees/${id}/pets`
+      );
+      setPets(response.data);
+    } catch (e) {
+      console.error("Error fetching pets:", e);
+    }
+  };
+
+  const petNames = pets.map((pet) => pet.name).join(", ");
+
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {pets.length > 0 ? (
+        <p>{petNames}</p>
+      ) : (
+        <p>No pets listed for this employee.</p>
+      )}
     </aside>
   );
 };
