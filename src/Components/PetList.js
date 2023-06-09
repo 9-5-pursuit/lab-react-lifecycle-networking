ef0a7f29b954cce308ca18ca97b2cadfdef62334
michael reyes <draftiercrayon4@gmail.com>
2023-06-09 13:58:24 -0400

    solved long standing cypress issue by adding a container around the petlist

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,7 +1,36 @@
-export const PetList = () => {
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export const PetList = ({ employeeId }) => {
+  const [petArray, setPetArray] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    handleFetchPets();
+  }, []);
+
+  async function handleFetchPets() {
+    try {
+      setIsFetching(true);
+
+      let result = await axios.get(
+        `https://vet-api-1.onrender.com/api/pets?employeeId=${employeeId}`
+      );
+
+      let dataArray = result.data.map((item) => item.name);
+
+      setPetArray(dataArray);
+      setIsFetching(false);
+    } catch (e) {}
+  }
+
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {!isFetching && petArray.length === 0 ? (
+        <p>No pets listed for this employee.</p>
+      ) : (
+        <p>{petArray.join(",")}</p>
+      )}
     </aside>
   );
 };
